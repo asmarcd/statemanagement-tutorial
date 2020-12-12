2a421d879e6c50fbe250207ed32a408f15d031ce
Asmar <asmarcd@gmail.com>
2020-12-11 16:14:31 -0800

    context api refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import MovieList from "./MovieList"
-import Nav from "./Nav"
+import MovieList from "./MovieList";
+import Nav from "./Nav";
+import { MovieProvider } from './MovieContext';
+import AddMovie from './AddMovie';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <MovieList />
-    </div>
+    <MovieProvider>
+      <div className="App">
+        <Nav />
+        <AddMovie />
+        <MovieList />
+      </div>
+    </MovieProvider>
   );
 }
 
